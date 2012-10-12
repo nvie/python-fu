091813a428be2c9494af70757b22eebbb0fcc320
Vincent Driessen <vincent@3rdcloud.com>
2012-10-12 23:55:11 +0200

    Be distutils2 compatible (future standard).

--- setup.py
+++ setup.py
@@ -1,57 +1,12 @@
-"""
-python-fu: Python command line tools, for increased fu.
-"""
-import sys
-from setuptools import setup, find_packages
-
-
-def get_dependencies():
-    deps = []
-    if sys.version_info < (2, 7):
-        deps += ['argparse']
-    return deps
-
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 setup(
-    name='python-fu',
-    version='0.1',
-    url='https://github.com/nvie/python-fu/',
-    license='BSD',
-    author='Vincent Driessen',
-    author_email='vincent@3rdcloud.com',
-    description=__doc__,
-    packages=find_packages(),
-    scripts=['bin/promote', 'bin/demote', 'bin/mkmodule'],
-    #include_package_data=True,
-    zip_safe=False,
-    platforms='any',
-    install_requires=get_dependencies(),
-    classifiers=[
-        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        #'Development Status :: 1 - Planning',
-        #'Development Status :: 2 - Pre-Alpha',
-        #'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
-        'Development Status :: 5 - Production/Stable',
-        #'Development Status :: 6 - Mature',
-        #'Development Status :: 7 - Inactive',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.3',
-        #'Programming Language :: Python :: 2.4',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: PyPy',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: System :: Systems Administration',
-    ]
+    setup_requires=['d2to1'],
+    d2to1=True
 )
